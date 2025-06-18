396bc8fe067f525106a141a1fa9b9d68e3885c01
Chaitanya Undale <137377022+chaitanya06041@users.noreply.github.com>
2025-06-19 01:06:24 +0530

    first commit

---
+++ src/Components/Graphs.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./CSS/Graphs.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  ZAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+  Legend,
+  ScatterChart,
+  Scatter,
+  LabelList,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
+import moment from "moment";
+import SlideCount from "./SlideCount";
+import PPTDistribution from "./PPTDistribution";
+import TopTags from "./TopTags";
+import SlidesPerFolder from "./SlidesPerFolder";
+import SlidesVsTags from "./SlidesVsTags";
+import FolderSummary from "./FolderSummary";
+import AverageSlides from "./AverageSlides";
+
+const Graphs = ({ filtered_data, folders }) => {
+  return (
+    <div className="graphs_container">
+      <SlideCount folders={folders} filtered_data={filtered_data} />
+      <PPTDistribution filtered_data={filtered_data} folders={folders} />
+      <TopTags filtered_data={filtered_data} />
+      <div className="row">
+        <SlidesPerFolder filtered_data={filtered_data} folders={folders} />
+        <AverageSlides filtered_data={filtered_data} folders={folders} />
+      </div>
+      <SlidesVsTags filtered_data={filtered_data} folders={folders} />
+      <FolderSummary filtered_data={filtered_data} folders={folders} />
+    </div>
+  );
+};
+
+export default Graphs;
